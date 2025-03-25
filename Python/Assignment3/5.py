e561a0474f818461c658b13eb422c89f10116c81
Sidhant Barik <suprcoolsidhant@gmail.com>
2025-03-25 19:48:14 +0530

    updated Assignment3 files

--- Python/Assignment3/5.py
+++
@@ -1,5 +0,0 @@
-import spacy
-nlp = spacy.load('en_core_web_sm')
-
-text = "Natural Language Processing enables machines to understand and process human languages. It is a fascinating field with numerous applications, such as chatbots and language translation."
-
